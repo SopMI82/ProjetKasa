adebd45511300b45a17a66b88ee117b7638605af
SopMI82 <sompydev@gmail.com>
2024-05-09 04:18:08 +0200

    scss : préparation
    mise en place des fichiers scss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
 const Card = () => {
   return (
-    <a href="#" className="housingCard">
-      <img src="" alt="photo du logement" />
-      <h3>titre</h3>
+    <a href="#">
+      <figure className="housingCard">
+        <img src="" alt="photo du logement" />
+        <h3>titre</h3>
+      </figure>
     </a>
   );
 };
