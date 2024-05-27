c855317dcc486e329819d8db61e7d2d4edafed29
SopMI82 <sompydev@gmail.com>
2024-05-27 15:13:22 +0200

    squelette Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,21 @@
 const Profile = () => {
     return (
-        <div>
-            <h2>Fiche Logement</h2>
+      <div>
+        <div className="carousel">
+
         </div>
+        <h2>Fiche Logement</h2>
+        <p>nknbcfknbkbnkbnkbnknb knvkmFWDNF</p>
+        <div className="host">
+          <p>;bfv,nbjvcnjcvbcjbjbjlcbnj!lbcnbcjl!nbc</p>
+          <img src="" alt="" />
+        </div>
+        <div className="description">
+            <title></title>
+            <i className="arrow"></i>
+            <p>jhgdjhgjlfhjbfjh</p>
+        </div>
+      </div>
     );
 };
 
