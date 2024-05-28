bb5872c8025af5dd960eea110128494de03d5a64
SopMI82 <sompydev@gmail.com>
2024-05-28 15:49:49 +0200

    construction de la page profile en cours

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,7 @@ import "./Card.scss";
 
 export const Card = (prop) => {
   return (
-    <Link
-      to={`profile/${prop.housing.id}`}
-      className="housingCard"
-    >
+    <Link to={`profile/${prop.housing.id}`} className="housingCard">
       <p>{prop.housing.title}</p>
       <img src={prop.housing.cover} alt="photo du logement" />
     </Link>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,22 +1,36 @@
-const Profile = () => {
-    return (
-      <div>
-        <div className="carousel">
+import { useParams } from "react-router-dom";
+import housings from "../../data/logements.json";
 
-        </div>
-        <h2>Fiche Logement</h2>
-        <p>nknbcfknbkbnkbnkbnknb knvkmFWDNF</p>
+const Profile = () => {
+  const { id } = useParams();
+  const house = housings.find((housing) => housing.id === id);
+  return (
+    <div>
+      <div className="carousel"></div>
+
+      <div className="description">
+        <h2>{house.title}</h2>
+        <p>{house.location}</p>
         <div className="host">
-          <p>;bfv,nbjvcnjcvbcjbjbjlcbnj!lbcnbcjl!nbc</p>
-          <img src="" alt="" />
-        </div>
-        <div className="description">
-            <title></title>
-            <i className="arrow"></i>
-            <p>jhgdjhgjlfhjbfjh</p>
+          <p>{house.host.name}</p>
+          <img src={house.host.picture} alt="photo de ${house.host.name}" />
         </div>
       </div>
-    );
+
+      <div className="tags">
+        {house.tags.map((tag, index) => {
+          return <div key={`${house.id}-tag-${index}`}>{tag}</div>;
+        })}
+      </div>
+
+      <div className="detail">
+        <title>{house.description}</title>
+        <i className="arrow"></i>
+        <p>jhgdjhgjlfhjbfjh</p>
+      </div>
+
+    </div>
+  );
 };
 
 export default Profile;
